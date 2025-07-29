bddc06e00a5d5fb239628b1b8ea8348806837377
sirMikeee <mike@stcecilia.edu.ph>
2025-07-29 13:30:08 +0800

    pusher

---
+++ src/appsecb/Appsecb.java
@@ -0,0 +1,9 @@
+package appsecb;
+
+public class Appsecb {
+
+    public static void main(String[] args) {
+        System.out.println("Activity 1");
+    }
+    
+}
---
+++ src/appsecb/activity2.java
@@ -0,0 +1,9 @@
+package appsecb;
+
+public class activity2 {
+    
+    public static void main(String[] args) {
+        System.out.println("Activity 2");
+    }
+    
+}
