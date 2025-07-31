bdae06bf0e0861d53aeaee669865c354201da1b5
sirMikeee <mike@stcecilia.edu.ph>
2025-07-31 13:54:03 +0800

    added banking class chuchu

--- src/appsecb/Appsecb.java
+++ src/appsecb/Appsecb.java
@@ -1,9 +1,52 @@
 package appsecb;
 
+import java.util.Scanner;
+import banking.bankingClass;
+
 public class Appsecb {
 
     public static void main(String[] args) {
-        System.out.println("Activity 1");
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("HELLO WELCOME TO THE SYSTEM!");
+        System.out.println("What do you feel doing today? ");
+        System.out.println("1. Banking");
+        System.out.println("2. Doctors Appointment");
+        System.out.println("3. Shopping");
+        
+        System.out.print("Enter choice: ");
+        int choice = sc.nextInt();
+        
+        switch(choice){
+            case 1:
+       
+                bankingClass bc = new bankingClass();
+                
+                System.out.println("Enter your Account No: ");
+                int accountNo = sc.nextInt();
+
+                System.out.println("Enter your Pin: ");
+                int pin = sc.nextInt();
+              
+                    if(bc.verifyAccount(accountNo, pin)){
+                        System.out.println("LOGIN SUCCESS");
+                    }else{
+                        System.out.println("INVALID ACCOUNT!");
+                    }
+                
+                
+                break;
+            case 2:
+                
+                break;
+            case 3:
+                
+                break;
+            default:
+                System.out.println("Invalid Selection!");
+        
+        }
+            
     }
     
 }
